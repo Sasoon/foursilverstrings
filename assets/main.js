7ca86e84852ca4912260f194c60e556af16db24c
Sasoon <sasoon91@gmail.com>
2021-07-02 00:16:50 +1000

    New pricing structure

--- assets/main.js
+++ assets/main.js
@@ -9,5 +9,15 @@ window.addEventListener('DOMContentLoaded', function(event) {
             button.attr('data-item-quantity', qty);
             price.text("$"+qty * base_price);
         }
+        if(evt.target.classList.contains('price-picker')){
+            console.log(evt.target);
+            var button = $('.buy-button');
+            var price = $('.price');
+            var base_price = button.data('item-price');
+            var qty = parseInt(evt.target.value);
+            console.log(qty);
+            button.attr('data-item-quantity', qty);
+            price.text("$"+qty * base_price);
+        }        
     })
 });
