7bd2f6fd67e919b934259c5006f5dd4fd3f82165
Sasoon <sasoon91@gmail.com>
2021-07-14 19:28:50 +1000

    Updated pricing, added images, added fields

---
+++ _paintings/sunflower.md
@@ -0,0 +1,19 @@
+---
+title: Sunflower Painting
+identifier: sunflower-painting
+description: This is the sunflower painting!
+price1: 10
+price2: 19
+price3: 28
+price4: 100
+price1-label: Small
+price2-label: Medium
+price3-label: Large
+paper-frame1-label: Paper 1
+paper-frame2-label: Paper 2
+thin-frame1-label: Thin 1
+thin-frame2-label: Thin 2
+thick-frame1-label: Thick 1
+thick-frame2-label: Thick 2
+image: /assets/images/flowers.jpg
+---
--- assets/main.js
+++ assets/main.js
@@ -2,23 +2,53 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     // globals
-    let buyButton = $('.buy-button');
-    let price = parseInt(buyButton.data('item-price'));
-    let priceText = $('.price');
+    var priceText = $('.price');
+    var sm = $('.buy-button--small');
+    var md = $('.buy-button--medium');
+    var lg = $('.buy-button--large');  
+    var btn = $('.buy-button');
+    var price = parseInt($('.buy-button:visible').data('item-price'));
+    
+    // default to medium
+    btn.hide();
+    md.show();
 
-    // Price handler
-    $('.price-picker').on('change', function() {
-        price = this.value;
-        // buyButton.attr('data-item-price', price);
-        buyButton.attr('data-item-quantity', 1);        
+    // frame handler
+    $('.frame-selector').on('change', function() {
+        frame = this.value;
+        console.log(frame);
+        $('.buy-button:visible').attr('data-item-custom1-value', frame);
+      });    
+
+    // size handler
+    $('.size-selector').on('change', function() {
+        size = this.value;
+        btn.hide();
+        switch (size) {
+          case "sm":
+            price = parseInt(sm.data('item-price'));
+            sm.show();
+            break;
+          case "md":
+            price = parseInt(md.data('item-price'));
+            md.show();
+            break;
+          case "lg":
+            price = parseInt(lg.data('item-price'));
+            lg.show();      
+            break;
+          default:
+            price = parseInt(md.data('item-price'));
+        }        
+        btn.attr('data-item-quantity', 1);              
         priceText.text("$"+ price);
         $('.qty').val(1);
       });
 
-    // Quantity handler
+    // quantity handler
     $('.qty').on('change', function() {
         var qty = this.value;
-        buyButton.attr('data-item-quantity', qty);
+        $('.buy-button:visible').attr('data-item-quantity', qty);
         priceText.text("$"+qty * price);
     });
 });
